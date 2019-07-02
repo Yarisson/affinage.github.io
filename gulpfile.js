1b490f0bd2e97ddc39073138e8232944258f79a9
Yarisson18 <gemini8811@gmail.com>
2019-07-02 06:36:18 +0300

    добавление файлов

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var htmlmin = require("gulp-htmlmin");
+var imagemin = require("gulp-imagemin");
+var posthtml = require("gulp-posthtml");
+var rename = require("gulp-rename");
+var include = require("posthtml-include");
+var del = require("del");
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "source/fonts/**/*",
+    "source/img/**",
+    "source/js/**"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("html", function() {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest("build"))
+    .pipe(server.stream());
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.scss", gulp.series("css"));
+  gulp.watch("source/*.html", gulp.series("html"));
+});
+
+gulp.task("build",
+gulp.series(
+"clean",
+"images",
+"copy",
+"css",
+"html"
+)
+);
